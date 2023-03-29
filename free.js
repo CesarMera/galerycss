7047f876d0fc38c4333a9ace717fec81ac66e01c
Cesar Mera <leo89mera@gmail.com>
2023-03-29 14:54:21 -0500

    GaleryCSS

---
+++ free.js
@@ -0,0 +1,14 @@
+// Configuración
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+
+firstLetterOfLastName = lastName[0]; 
+console.log(firstLetterOfLastName);
